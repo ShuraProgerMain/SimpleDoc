8475e6572cc1aa05acbdf563133bfff1deb40b2c
ShuraProger <stupidshitcreate@gmail.com>
2023-11-12 19:33:34 +0300

    Add game builder

---
+++ Assets/Editor/GameBuilder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Editor
+{
+    public class GameBuilder
+    {
+        [MenuItem("CI / Windows Build")]
+        public static void GitWindowsBuild()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions()
+            {
+                scenes = new []{ "Assets/Scenes/SampleScene.unity" },
+                locationPathName = "build/Windows",
+                target = BuildTarget.StandaloneWindows64,
+                options = BuildOptions.None
+                
+            };
+
+            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            var summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+    }
+}
